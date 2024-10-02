78e3dd1be1270c05a948735acaf0d2639d1a54e2
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-02 13:41:31 +0300

    feat: add test removeWords

---
+++ src/test/test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+const { removeWords, getMostPopularPlaces } = require('../src/functions');
+
+describe('removeWords', function() {
+    it('should remove specified words from the string', function() {
+      const result = removeWords('Hello, ceci est un test', ['ceci', 'un']);
+      expect(result).to.equal('Hello, est test');
+    });
+})
