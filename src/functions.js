d3aebe345a8bcc6876df79a6a5ae7263ff1c1bcf
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-02 14:42:43 +0300

    feat: implement function getMostPopularPlaces .

--- src/functions.js
+++ src/functions.js
@@ -5,3 +5,15 @@ export function removeWords(original, wordsToRemove) {
     .filter(word => !wordsToRemove.includes(word))
     .join(' ');
 }
+export function getMostPopularPlaces(...places) {
+  const countMap = places.reduce((acc, place) => {
+    acc[place] = (acc[place] || 0) + 1;
+    return acc;
+  }, {});
+
+  const sortedPlaces = Object.entries(countMap)
+    .sort((a, b) => b[1] - a[1])
+    .map(entry => entry[0]);
+
+  return `${sortedPlaces[0]} ${sortedPlaces[1]}`;
+}
