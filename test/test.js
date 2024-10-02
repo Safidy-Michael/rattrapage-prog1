add27dd08b145b1e34f67f84bab75cff0889c3d0
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-02 14:58:00 +0300

    chore: adding more test for removeWords.

--- test/test.js
+++ test/test.js
@@ -6,7 +6,13 @@ describe('removeWords', function() {
     const result = removeWords('Hello, ceci est un test', ['ceci', 'un']);
     expect(result).to.equal('Hello, est test');
   });
+});
 
+describe('removeWords', function() {
+  it('should remove specified words from the string', function() {
+    const result = removeWords('Hello Java', ['java']);
+    expect(result).to.equal('Hello Java');
+  });
 });
 
 describe('getMostPopularPlaces', function() {
