22c14fe77df2b9dae1840f7d75479ae605430610
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2024-10-02 14:51:52 +0300

    feat: implement test for getMostPopularPlaces .

--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
-const { expect } = require('chai');
-const { removeWords, getMostPopularPlaces } = require('../src/functions');
+import { expect } from 'chai';
+import { removeWords, getMostPopularPlaces } from '../src/functions.js';
 
 describe('removeWords', function() {
-    it('should remove specified words from the string', function() {
-      const result = removeWords('Hello, ceci est un test', ['ceci', 'un']);
-      expect(result).to.equal('Hello, est test');
-    });
-})
+  it('should remove specified words from the string', function() {
+    const result = removeWords('Hello, ceci est un test', ['ceci', 'un']);
+    expect(result).to.equal('Hello, est test');
+  });
 
+});
+
+describe('getMostPopularPlaces', function() {
+  it('should return the two most popular places', function() {
+    const result = getMostPopularPlaces('Analamahitsy', 'Analamahitsy', 'Itaosy', 'Behoririka', 'Itaosy', 'Itaosy');
+    expect(result).to.equal('Itaosy Analamahitsy');
+  });
+})
